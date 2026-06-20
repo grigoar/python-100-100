e73cb92c34b40617341717d3b24c5e66d0641e61
grigoar <grigor.nath@gmail.com>
2026-06-20 18:16:23 +0300

    day one input print

---
+++ day1/main.py
@@ -0,0 +1,66 @@
+# print("hello world!")
+
+# print ("Day 1 - Python Print Function")
+# print ("The function is declared like this:")
+# print ("print(\"what to print\")")
+
+# print ("Hello world \nHello world")
+# print ("Hello" + " " + " Grigore")
+# print ("Hello" + " " + " Grigore")
+
+# input("What is your name?")
+
+# name = input ("What is your name?")
+# print(name)
+
+# name = "Grigore"
+# print(name)
+
+# # The input will be run first, then the print will be executed.
+# print("Hello " + input("What is your name? ") + "!")
+
+# Lenght of a string
+# print(len(input("What is your name?")))
+
+# Variables
+# name = input("What is your name?")
+# length = len(name)
+# print(length)
+
+# a = input("a: ")
+# b = input("b: ")
+
+# temp = b
+# b=a
+# a=temp
+
+# print("switched values:")
+# print("a= " + a)
+# print("b= " + b)
+
+# print(justified_text)
+
+# naming conventions - make them readable
+# snake_case
+# camelCase
+# PascalCase
+
+# variables
+# variables are used to store data
+# variables are used to store data in a program
+# variables are used to store data in a program
+
+# Band Name Generator
+# 1. Create a greeting for your program.
+print("welcome to the band name generator.")
+# 2. Ask the user for the city that they grew up in.
+city_name= input("What's name of the city you grew up in?\n")
+# print(city_name)
+# 3. Ask the user for the name of a pet.
+pet_name = input("What's your pet's name?\n")
+# print(pet_name)
+# 4. Combine the name of their city and pet and show them their band name.
+print("Your band name could be " + city_name + " " + pet_name)
+# 5. Make sure the input cursor shows on a new line, see the example at:
+# https://replit.com/@appbrewery/band-name-generator-end
+
