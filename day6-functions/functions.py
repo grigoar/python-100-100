16bfdf91ddead586383f69cdbc11747505768f8f
grigoar <grigor.nath@gmail.com>
2026-06-27 17:48:51 +0300

    day 6 - functions, loops, code blocks

---
+++ day6-functions/functions.py
@@ -0,0 +1,34 @@
+
+# #? define a function
+# print("Hello")
+# num_char = len("Hello")
+# print(num_char)
+
+# def my_function():
+#   print("Hello")
+#   print("Bye")
+
+# my_function()
+
+#? Indentation - tabs vs spaces
+#? While loop
+
+# number_of_jumps = 6
+
+
+def jump():
+  print("jump")
+
+# while number_of_jumps >0:
+#   jump()
+#   number_of_jumps-=1
+#   print(number_of_jumps)
+
+
+
+# at_goal=False
+
+# while not at_goal:
+#   jump()
+#   at_goal = input("enter True or False for at_goal: ").lower() == "true"
+  
