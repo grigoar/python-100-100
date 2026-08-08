fc1795d52bf5e22fb351c9e864ab91f949453265
grigoar <grigor.nath@gmail.com>
2026-08-08 18:45:11 +0300

    day 28-29 tkinker GUI and dynamic typing

---
+++ day28-29-dynamic-typing-tkinter/main.py
@@ -0,0 +1,18 @@
+# Dynamic Typing Pomodoro
+
+# 1. Define the main function
+def main():
+    # 2. Define the variables
+    name = "John"
+    age = 20
+    is_student = True
+
+    # 3. Print the variables
+    print(name, age, is_student)
+
+
+# 4. Call the main function
+main()
+# 5. Run the program
+if __name__ == "__main__":
+    main()
